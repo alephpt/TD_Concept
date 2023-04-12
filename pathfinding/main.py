90d15d2485ebfaf914c8d76ee11b7f9cf54deb91
alephpt <alephpt@github.com>
2023-04-12 16:36:45 -0500

    map generation

--- pathfinding/main.py
+++ pathfinding/main.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_gui
 import math
 import random
 import noise
@@ -31,10 +32,9 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 class Map:
     def __init__(self):
         self.squares = []
-        self.generate_map()
-        self.generate_points()
+        # self.generate_points()
         
-    def generate_map(self):
+    def blank_map(self):
         for y in range(N_MAP_SQUARES_Y):
             for x in range(N_MAP_SQUARES_X):
                 self.squares.append(Square(screen, CENTER_X, CENTER_Y, x, y, SQUARE_SIZE, Color.Gray, True, 1))
@@ -52,47 +52,107 @@ class Map:
         end.fill = 0
         end.path = False
 
-    def generate_noise(self):
+    def generate_noise(self, sliders):
+        self.blank_map()
+        
         base = random.randint(0, 4096)
+        
+        scale = sliders[0].current_value
+        octaves = sliders[1].current_value
+        persistence = sliders[2].current_value
+        lacunarity = sliders[3].current_value
         
         for y in range(N_MAP_SQUARES_Y):
             for x in range(N_MAP_SQUARES_X):
-                if snoise2(x, y, 1, base=base) > 0.5:
-                    self.squares[Square(screen, CENTER_X, CENTER_Y, x, y, SQUARE_SIZE, Color.Gray, False, 1)]
+                if noise.snoise2(x * scale, y * scale, octaves=octaves, persistence=persistence, lacunarity=lacunarity, base=base) > 0.375:
+                    self.squares[y * N_MAP_SQUARES_X + x] = Square(screen, CENTER_X, CENTER_Y, x, y, SQUARE_SIZE, Color.Gray, False, 0)
+                else:
+                    self.squares[y * N_MAP_SQUARES_X + x] = Square(screen, CENTER_X, CENTER_Y, x, y, SQUARE_SIZE, Color.Gray, True, 1)
 
 
 class Game:
     def __init__(self):
         self.running = True
+        self.ui = UI()
         self.map = Map()
+        self.map.generate_noise(self.ui.sliders)
+        
         
     def draw(self, xloc, yloc, zoom):
         for square in self.map.squares:
             square.draw(xloc, yloc, zoom)
+
+    def update(self):
+        self.ui.update(self)
+
+class UI:
+    def __init__(self):
+        self.camera = Camera(MAP_WIDTH, MAP_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.labels = []
+        self.values = []
+        self.sliders = []
+        self.create_sliders()
+        
+    def create_sliders(self):
+        # scale slider
+        self.labels.append(pygame_gui.elements.UILabel(pygame.Rect((30, 20), (50, 20)), "Scale", self.manager, object_id='#scale_label'))
+        self.sliders.append(pygame_gui.elements.UIHorizontalSlider(pygame.Rect((30, 40), (200, 20)), 0.1, (0.1, 1.0), self.manager, object_id='#scale_slider'))
+        self.values.append(pygame_gui.elements.UILabel(pygame.Rect((120, 20), (60, 20)), str(self.sliders[0].current_value), self.manager))
+        # octaves slider
+        self.labels.append(pygame_gui.elements.UILabel(pygame.Rect((30, 60), (65, 20)), "Octaves", self.manager, object_id='#octaves_label'))
+        self.sliders.append(pygame_gui.elements.UIHorizontalSlider(pygame.Rect((30, 80), (200, 20)), 1, (1, 10), self.manager, object_id='#octaves_slider'))
+        self.values.append(pygame_gui.elements.UILabel(pygame.Rect((120, 60), (60, 20)), str(self.sliders[1].current_value), self.manager))
+        # persistence slider
+        self.labels.append(pygame_gui.elements.UILabel(pygame.Rect((30, 100), (90, 20)), "Persistence", self.manager, object_id='#persistence_label'))
+        self.sliders.append(pygame_gui.elements.UIHorizontalSlider(pygame.Rect((30, 120), (200, 20)), 1.5, (0.1, 3.0), self.manager, object_id='#persistence_slider'))
+        self.values.append(pygame_gui.elements.UILabel(pygame.Rect((120, 100), (60, 20)), str(self.sliders[2].current_value), self.manager))
+        # lacunarity slider
+        self.labels.append(pygame_gui.elements.UILabel(pygame.Rect((30, 140), (85, 20)), "Lacunarity", self.manager, object_id='#lacunarity_label'))
+        self.sliders.append(pygame_gui.elements.UIHorizontalSlider(pygame.Rect((30, 160), (200, 20)), 2.6, (0.1, 3.0), self.manager, object_id='#lacunarity_slider'))
+        self.values.append(pygame_gui.elements.UILabel(pygame.Rect((120, 140), (60, 20)), str(self.sliders[3].current_value), self.manager))
     
+    def update_values(self):
+        # scale slider value
+        self.values[0].set_text(str(self.sliders[0].current_value))
+        # octaves slider value
+        self.values[1].set_text(str(self.sliders[1].current_value))
+        # persistence slider value
+        self.values[2].set_text(str(self.sliders[2].current_value))
+        # lacunarity slider value
+        self.values[3].set_text(str(self.sliders[3].current_value))
+    
+    def update(self, game):
+        self.camera.render(game)
+        self.manager.update(clock.get_time())
+        self.manager.draw_ui(screen)
 
 # main function
 def main():
     game = Game()
-    camera = Camera(MAP_WIDTH, MAP_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT)
     
     while game.running:
         for e in pygame.event.get():
+            game.ui.manager.process_events(e)
             if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE):
                 game.running = False
             elif e.type == pygame.MOUSEBUTTONDOWN:
                 if e.button == 4:
-                    new_zoom = camera.zoom * 1.1
-                    camera.zoom = min(camera.max_zoom, new_zoom)
+                    new_zoom = game.ui.camera.zoom * 1.1
+                    game.ui.camera.zoom = min(ui.camera.max_zoom, new_zoom)
                 elif e.button == 5:
-                    new_zoom = camera.zoom / 1.1
-                    min_zoom = camera.minZoom()
-                    camera.zoom = max(min_zoom, new_zoom)
+                    new_zoom = game.ui.camera.zoom / 1.1
+                    min_zoom = game.ui.camera.minZoom()
+                    game.ui.camera.zoom = max(min_zoom, new_zoom)
+            elif e.type == pygame.USEREVENT:
+                if e.user_type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
+                    if e.ui_element in game.ui.sliders:
+                        game.map.generate_noise(game.ui.sliders)
+                        game.ui.update_values()
 
         screen.fill(Color.Black.value)
 
-        camera.update()
-        camera.render(game)
+        game.update()
 
         pygame.display.update()
         clock.tick(60)
